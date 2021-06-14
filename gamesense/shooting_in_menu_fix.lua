b415aabf1be69283a6512749480f8b38b80fca9d
Envy <mouse5holder@gmail.com>
2021-06-14 19:18:28 -0400

    Update shooting_in_menu_fix.lua

--- gamesense/shooting_in_menu_fix.lua
+++ gamesense/shooting_in_menu_fix.lua
@@ -1,7 +1,6 @@
 -- local variables for API functions. any changes to the line below will be lost on re-generation
-local ui_is_menu_open, ui_reference, ui_set = ui.is_menu_open, ui.reference, ui.set
+local ui_is_menu_open, ui_reference, ui_set,client_set_event_callback = ui.is_menu_open, ui.reference, ui.set,client.set_event_callback
 
-local client_set_event_callback = client.set_event_callback
 local legitbot_toggle = ui_reference('LEGIT', 'Aimbot', 'Enabled')
 client_set_event_callback('setup_command', function(e) 
     if ui_is_menu_open() then 
