c6791222f7f454161d099ce53fd8dc57fae5065a
Envy <mouse5holder@gmail.com>
2021-06-14 19:17:49 -0400

    Update shooting_in_menu_fix.lua
    updated to use cached api calls. added a check for in menu for the legit bot since it aims if the menus open.

--- gamesense/shooting_in_menu_fix.lua
+++ gamesense/shooting_in_menu_fix.lua
@@ -1,2 +1,12 @@
+-- local variables for API functions. any changes to the line below will be lost on re-generation
+local ui_is_menu_open, ui_reference, ui_set = ui.is_menu_open, ui.reference, ui.set
+
 local client_set_event_callback = client.set_event_callback
-client_set_event_callback("setup_command", function(e) if ui.is_menu_open() then e.in_attack = false e.in_attack2 = false end end)
+local legitbot_toggle = ui_reference('LEGIT', 'Aimbot', 'Enabled')
+client_set_event_callback('setup_command', function(e) 
+    if ui_is_menu_open() then 
+        e.in_attack = false 
+        e.in_attack2 = false 
+    end
+    ui_set(legitbot_toggle,not ui_is_menu_open())
+end)
